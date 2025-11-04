0856faf26841781c42639b6bf570e8d88ddc992f
Gregory Pellechi <greg.pellechi@gmail.com>
2025-11-04 10:24:55 +0200

    Updated node version and line to point to it correctly

--- init.lua
+++ init.lua
@@ -211,6 +211,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+-- Ensure Neovim uses the correct Node.js version
+vim.env.PATH = os.getenv 'HOME' .. '/.nvm/versions/node/v20.19.5/bin:' .. vim.env.PATH
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -633,7 +635,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
